87148a2f89e9003cc9de4283930b06a661f819de
gracebir <grace.devolop@gmail.com>
2023-02-14 20:12:07 +0200

    config the site color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode:"class",
   theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors:{
+      textLight: 'var(--color-text-light)',
+      textDark: 'var(--color-text-dark)',
+      textInputdark: 'var(--color-text-dark)'
+    },
+    backgroundColor:{
+      bgLight: 'var(--color-bg-light)',
+      bgDark: 'var(--color-bg-light)',
+    },
     extend: {},
   },
   plugins: [],
