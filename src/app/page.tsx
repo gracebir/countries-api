c8a72ad3c4a710122285351258e528c089b7f98c
gracebir <grace.devolop@gmail.com>
2023-08-17 16:45:28 +0200

    fetching data on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
-export default async function Home() {
+import { baseURI } from "@/axios"
+import Countries from "@/components/Countries";
+import { countryResponse } from "../../type";
 
+async function getCountries() {
+  const response = await baseURI.get('/all');
+  const data: Array<countryResponse> = await response.data
+  return data
+}
+
+export default async function Home() {
+  const data = await getCountries()
   return (
     <main>
-      country api
+      <Countries countries={data}/>
     </main>
   )
 }
