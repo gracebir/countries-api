b6e7044a04fab569048e05a870ef3d4274f9fae1
gracebir <grace.devolop@gmail.com>
2023-08-17 16:43:37 +0200

    countries details page

---
+++ src/app/country/[name]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Country = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-red-500">Country details</h1>
+    </div>
+  )
+}
+
+export default Country
